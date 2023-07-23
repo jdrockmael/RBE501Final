83b4a4c1ccbe210a46bd78df7f94b1846d275517
Jason Rockmael <jdrockmael@wpi.edu>
2023-07-23 09:54:09 -0700

    trajectory planning implemented, but robot is freaking out again, so back to PID tuning

--- RBE501Init.m
+++ RBE501Init.m
@@ -3,15 +3,31 @@ clc; clear; close all
 global robot dof jointTargetPos jointTargetVel 
 robot = importrobot('irb1600id.urdf','DataFormat','column');               % load robot model, set data format to column, set gravity vector
 robot.Gravity = [0 0 -9.8];
+planner = Traj_Planner();
 dof = numel(homeConfiguration(robot));                                     % get robot degree of freedom
-jointInitialPos_Vel = [0,0,0,0,pi/3,0,0,0,0,0,0,0]';                       % define initial joint angles to be [0,0,0,0,0,0,0,0,0,0,0,0]
-jointTargetPos = [pi/6, pi/6, pi/6, 0, pi/2, 0]';                          % define desired joint angles. 
+jointInitialPos_Vel = [0,0,0,0,pi/6,0,0,0,0,0,0,0]';                       % define initial joint angles to be [0,0,0,0,0,0,0,0,0,0,0,0]
+jointTargetPos = [pi/6, pi/6, pi/6, 0, 0.33, 0]';                          % define desired joint angles. 
 jointTargetVel = [0, 0, 0, 0, 0, 0]';
-Tf = 0.5;                                                                  % simulation end time
+Tf = 0.6;                                                                  % simulation end time
 tSpan = [0, Tf];                                                           % define simulation time span
 tic;                                                                       % benchmarking
 [T, X] = ode45(@(t,x)armODE(t,x),tSpan,jointInitialPos_Vel);               % solve robot dynamical model dq=F(q,dq), robot state space is defined as X=[q, dq]
 toc;
+
+traj_Mat1 = planner.quintic_traj(0, Tf, jointInitialPos_Vel(1,:),...       % trajectory planning function at each joint
+    jointTargetPos(1), jointInitialPos_Vel(7,:), jointTargetVel(1), 0, 0);
+traj_Mat2 = planner.quintic_traj(0, Tf, jointInitialPos_Vel(2,:),...
+    jointTargetPos(2), jointInitialPos_Vel(8,:), jointTargetVel(2), 0, 0);
+traj_Mat3 = planner.quintic_traj(0, Tf, jointInitialPos_Vel(3,:),...
+    jointTargetPos(3), jointInitialPos_Vel(9,:), jointTargetVel(3), 0, 0);
+traj_Mat4 = planner.quintic_traj(0, Tf, jointInitialPos_Vel(4,:),...
+    jointTargetPos(4), jointInitialPos_Vel(10,:), jointTargetVel(4), 0, 0);
+traj_Mat5 = planner.quintic_traj(0, Tf, jointInitialPos_Vel(5,:),...
+    jointTargetPos(5), jointInitialPos_Vel(11,:), jointTargetVel(5), 0, 0);
+traj_Mat6 = planner.quintic_traj(0, Tf, jointInitialPos_Vel(6,:),...
+    jointTargetPos(6), jointInitialPos_Vel(12,:), jointTargetVel(6), 0, 0);
+
+traj_Mat = [traj_Mat1,traj_Mat2,traj_Mat3,traj_Mat4,traj_Mat5,traj_Mat6];  % combine joints
 
 %% animation
 figure()                                                                   % create new figure and set figure properties
@@ -21,7 +37,7 @@ view(60,10);                                                               % set
 hold on
 interval = round(0.01*length(X));                                          % set animation update interval (we have too many states)
 for i = 1:interval:length(X)
-    jointPos = X(i,1:dof);                                                 % get current joint positions from state space
+    jointPos = X(i,1:dof)*traj_Mat;                                        % get current joint positions from state space
     show(robot,jointPos','PreservePlot',false);                            % show robot at current joint configuration
     title(sprintf('Frame = %d of %d', i, length(X)));                      % set figure title
     xlim([-1,1]); ylim([-1,1]); zlim([0,2]);                               % limitaxis range
@@ -74,7 +90,7 @@ end
 
 function tau = jointPD(joint_target_pos,joint_target_vel,x)
 global dof
-   Kp = 25000;
+   Kp = 10000;
    Ki = 10000;
    Kd = 3500;
    t1 = (joint_target_pos(1)-x(1))*Kp + Ki*cumtrapz((joint_target_pos(1)-x(1)),1) + (joint_target_vel(1)-x(7))*Kd;
